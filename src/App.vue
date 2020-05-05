41592f907c236c860541661e3d7362646380b3f9
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-05-05 19:25:08 +0200

    Title change for neutral for now

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="yacht-charter-app">
-    <div :any="any" class="smth">Jakiś tekst tylko na próbę</div>
-    <nav></nav>
+    <!-- <div :any="any" class="smth">Jakiś tekst tylko na próbę</div> -->
+    <TheNavbar />
   </div>
 </template>
 
 <script>
+import TheNavbar from "@/components/TheNavbar";
 export default {
   name: "App",
-  components: {}
+  components: { TheNavbar }
 };
 </script>
 
 <style>
 #yacht-charter-app {
+  /* Czcionkę możemy sobie zmienić jak co także mus to nie jest może znajdziemy jakąś fajną pod tą stronę */
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+/* All elements */
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+/* Navbar styles - później przeniesione zostaną do TheNavbar.vue component */
 </style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main__navigation">
+    <!-- <h1>That is the main navigation component:)</h1> -->
+    <nav class="main-navigation">
+      <a href="#yacht-charter-app">
+        <img src="../assets/images/logo-icon/1.blue.svg" alt="Sailing Yacht" />
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
