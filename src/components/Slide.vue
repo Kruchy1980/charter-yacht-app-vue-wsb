6c57fc86893c5eb93955779f60acb45e102bf3ab
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-05-07 18:49:10 +0200

    Initializing of slider - check out if the assignemts working

---
+++ src/components/Slide.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="slide__box">
+    <!-- And add here displaying of slide -->
+    <!-- The slide id is: {{ slide.id }} with source of: {{ slide.src }} -->
+    <div class="slide__box__image"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  // Here we are adding props - oassed from TheSlider.vue component where we have binded the slide as :slide="slides[0]" with all of the properties of a slide
+  props: ["slide"]
+};
+</script>
+
+<style lang="scss" scoped>
+.slide__box {
+  margin: 30px 0;
+  height: 50vh;
+  border: 1px solid #000;
+
+  &__image {
+    // just for check
+    background-image: url("../assets/images/slider-image/sailing/4.yacht-s.jpeg");
+    height: 100%;
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+}
+// .slide__box__image {
+//   // just for check
+//   // background-image: url("../assets/images/slider-image/sailing/4.yacht-s.jpeg");
+//   height: 100%;
+//   width: 100%;
+//   background-position: center;
+//   background-repeat: no-repeat;
+//   background-size: cover;
+// }
+</style>
--- src/components/TheSlider.vue
+++ src/components/TheSlider.vue
@@ -1,12 +1,47 @@
 <template>
+  <!-- First bind the images to the slide component -->
   <div class="slider__box">
-    <h1>The slider will be here:)</h1>
+    <Slide :slide="slides[0]" />
   </div>
 </template>
 
 <script>
-export default {};
+import Slide from "@/components/Slide";
+export default {
+  // Adding the slides to the TheSlider Components zn separate obects
+  components: { Slide },
+
+  data() {
+    return {
+      slides: [
+        {
+          id: 0,
+          src: "../assets/images/slider-image/sailing/1.yacht-s.jpeg"
+        },
+        {
+          id: 1,
+          src: "../assets/images/slider-image/motor/1.motorboat.jpg"
+        },
+        {
+          id: 2,
+          src:
+            "../assets/images/slider-image/catamaran-sailing/1.catamaran-s.jpeg"
+        },
+        {
+          id: 3,
+          src:
+            "../assets/images/slider-image/catamaran-motor/2.catamaran-m.jpeg"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.slider__box {
+  h1 {
+    text-align: center;
+  }
+}
 </style>
