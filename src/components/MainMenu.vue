a20ce9413225e879a93669adc193309ceae8da0e
Paweł Jarosiewicz <pawlojar@gmail.com>
2020-05-12 20:55:35 +0200

    main menu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,23 +1,32 @@
 <template>
-<header class="main-header primary-color">
-    <div class="main-header-nav">
-        <a :href=mainUrl ><img :src=logoUrl alt="logo" class="nav-logo"></a>
-        <button type="button" class="nav-toggler"><i class="fas fa-bars fa-lg"></i><i class="fas fa-times fa-lg "></i></button>  
-        <nav>
-            <ul class="nav-menu">
-                <li><a href="" class="nav-link primary-color">Strona główna</a></li>
+<header class="main_header primary_color">
+    <div class="main_header_nav">
+        <a :href=mainUrl ><img :src=logoUrl alt="logo" class="nav_logo"></a>
+        <button type="button" class="nav_toggler" v-on:click="showMobileMenu()">
+            <i class="fas fa-bars fa-lg" v-show="isMobileMenuClicked"></i>
+            <i class="fas fa-times fa-lg " v-show="!isMobileMenuClicked"></i>
+        </button>  
+        <nav class="hide">
+            <ul class="nav_menu">
+                <li><a href="" class="nav_link primary_color">Strona główna</a></li>
                 <li>
-                    <a href="" class="nav-link primary-color">Czartery jachtów</a>
-                    <!-- <ul>
-                        <li><a href="">Jachty żaglowe</a></li>
-                        <li><a href="">Jachty motorowe</a></li>
-                        <li><a href="">Katamarany żaglowe</a></li>
-                        <li><a href="">Katamarany motorowe</a></li>
-                    </ul> -->
+                    <a type="button" class="nav_link primary_color" v-on:click="expandMenuCharter($event)">Czartery jachtów 
+                        <i class="fas fa-caret-down" v-show="!isExpanded"></i>
+                        <i class="fas fa-caret-up" v-show="isExpanded"></i>
+                    </a>
+                    <ul class="nav_dropdown hide">
+                        <li><a href="" class="nav_link primary_color">Jachty żaglowe</a></li>
+                        <li><a href="" class="nav_link primary_color">Jachty motorowe</a></li>
+                        <li><a href="" class="nav_link primary_color">Katamarany żaglowe</a></li>
+                        <li><a href="" class="nav_link primary_color">Katamarany motorowe</a></li>
+                    </ul>
                 </li>
-                <li><a href="" class="nav-link primary-color">Kontakt</a></li>
-                <li><a href="" class="nav-link primary-color">O Nas</a></li>
-                <li><a href="" class="nav-link primary-color"><i class="fas fa-user-circle fa-lg"></i></a></li>
+                <li><a href="" class="nav_link primary_color">Kontakt</a></li>
+                <li><a href="" class="nav_link primary_color">O Nas</a></li>
+                <li><a href="" class="nav_link primary_color">
+                    <i class="fas fa-user-circle fa-lg"></i>
+                    </a>
+                </li>
             </ul>
         </nav>
     </div>
@@ -29,17 +38,28 @@
 export default {
     data(){
         return{
-            mainUrl: '',
-            logoUrl: './bookmark-icon/1.blue.svg',
+            mainUrl: '/',
+            logoUrl: '/bookmark-icon/1.blue.svg',
+            isMobileMenuClicked: true,
+            isExpanded: false,
+        }
+    },
+    methods: {
+        showMobileMenu(){ 
+            this.isMobileMenuClicked=!this.isMobileMenuClicked; 
+            let nav = document.getElementsByTagName('nav')[0];
+            nav.classList.toggle('show');
+            },
+        expandMenuCharter(e){
+            this.isExpanded=!this.isExpanded;
+            let ul = e.target.nextSibling
+            ul.classList.toggle('show');
         }
     }
 }
 </script>
 
 <style scoped>
-nav{
-    margin: auto 5% auto auto;
-}
 ul{
     margin: 0;
     padding: 0;
@@ -48,11 +68,26 @@ ul{
 ul a{
     text-decoration: none;
 }
-.nav-link{
-    padding-left: .7rem;
-    padding-right: .7rem;
+nav{
+    flex-basis: 100%;
 }
-.nav-toggler{
+li{
+    padding-left: .5rem;
+    margin-top: .5rem;
+}
+.nav_link{
+    padding: .25rem;
+    border: solid 1px transparent;
+    transition: all .5s;
+}
+.nav_link:hover{
+    cursor: pointer;
+    border-radius: 7px;
+    border-left: solid 1px rgba(60, 84, 180,.6);
+    border-right: solid 1px rgba(60, 84, 180,.6);
+    color: rgba(60, 84, 180,.6);
+}
+.nav_toggler{
     height: 2rem;
     width: 2rem;
     margin: auto 0 auto auto;
@@ -64,7 +99,21 @@ ul a{
     text-align: center;
     transition: all .5s;
 }
-.main-header{
+.nav_menu{
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    margin-bottom: .5rem;
+}
+.nav_dropdown{
+    position: static;
+    width: fit-content;
+    padding: 3px;
+    border-radius: 7px;
+    border-bottom: 1px solid rgb(102,102,102, .2);
+    background-color: #fff;
+}
+.main_header{
     margin: 0px;
     padding: 0px;
     position: relative;
@@ -73,15 +122,16 @@ ul a{
     background-color: #fff;
     z-index: 1000;
     display: flex;
-    /* transition: opacity .3s, background-color .3s; */
+    border-bottom: 1px solid rgb(102,102,102, .2);
 }
-.main-header-nav{
+.main_header_nav{
     margin: 0 auto;
     width: 1200px;
     max-width: 90%;
     display: flex;
+    flex-wrap: wrap;
 }
-.nav-logo{
+.nav_logo{
     display: inline-block;
     padding-top: .3rem;
     padding-bottom: .3rem;
@@ -91,7 +141,9 @@ ul a{
     white-space: nowrap;
     height: 3rem;
 }
-
+.hide:not(.show){
+    display: none;
+}
 
 /* szkielet dla różnych wyświetlaczy */
 @media (min-width: 640px){
@@ -99,16 +151,34 @@ ul a{
 }
  
 @media (min-width: 768px){
-    .nav-toggler{
+    nav{
+        margin: auto 5% auto auto;
+        display: inherit !important;
+        flex-basis: auto;
+    }
+    li{
+        margin-top: 0.25rem;
+        margin-bottom: .25rem;
+    }
+    .main_header_nav{
+        flex-direction: row;
+        flex-wrap: nowrap;
+    }
+    .nav_toggler{
         display: none;
+    }
+    .nav_menu{
+        display: flex;
+        flex-direction: row;
+    }
+    .nav_dropdown{
+        position: absolute;
+        width: auto;
     }
 }
  
 @media (min-width: 1024px){
- .nav-menu{
-    display: flex;
-    flex-direction: row;
-}
+    
 }
  
 @media (min-width: 1200px){
