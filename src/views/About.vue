13b4192afd263d840ae14947cc1d8a91e0190c81
Paweł Jarosiewicz <pawlojar@gmail.com>
2020-05-15 21:37:37 +0200

    fix routing: default route, remove hash from url

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="about">
+    <div id="about">
         <MainMenu />
+        <div class=" primary_color">
         <h2>O nas</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam necessitatibus, dicta corporis consectetur ipsa commodi, nostrum velit animi in non quia nisi, ducimus voluptates soluta libero debitis tempore officiis odit?</p>
+        </div>
+        
         <MainFooter />
     </div>
 </template>
@@ -17,4 +21,26 @@ export default {
 
 <style>
 
+#about::after {
+  content: "";
+  background:  url("/images/background/about-small.jpg") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  opacity: 0.5;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+  z-index: -1;  
+  height: 100vh;  
+}
+
+@media (min-width: 768px){
+    #about::after {
+        background:  url("/images/background/about-big.jpg") no-repeat center center fixed;;
+    }
+}
 </style>
