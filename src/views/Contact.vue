4e315e46983153f4837e3fc0865032ba7afd1b94
Paweł Jarosiewicz <pawlojar@gmail.com>
2020-05-21 14:32:53 +0200

    add contact form without sending msg

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,8 +6,24 @@
         <article class="contact__article text__color--dark">
           <h2 class="contact__article__title">Kontakt</h2>
           <p class="contact__article__p">Napisz do nas! Z góry dziękujemy za wszystkie sugestie i opinie.</p>
-          <form action="submit" class="contactform">
-
+          <form action="" class="contactform" >
+              <div class="contactform__group">
+                  <label class="contactform__label" for="name">Imię <span class="text__color--gray">(wymagane)</span></label>
+                  <InputText id="name" :minLength=3 :errorText="'Podaj swoje imię, minimum 3 znaki.'" ref="name"/>
+              </div>
+              <div class="contactform__group">
+                  <label class="contactform__label" for="email">E-mail <span class="text__color--gray">(wymagane)</span></label>
+                  <InputEmail id="mail" :errorText="'Proszę podać poprawny adres e-mail.'" ref="email"/>
+              </div>
+              <div class="contactform__group">
+                  <label class="contactform__label" for="subject">Temat <span class="text__color--gray">(wymagane)</span></label>
+                  <InputText id="subject" :minLength=1 :errorText="'Określ temat wiadomości.'" ref="subject"/>
+              </div>
+              <div class="contactform__group">
+                  <label class="contactform__label" for="msg">Treść wiadomości <span class="text__color--gray">(wymagane)</span></label>
+                  <InputTextArea id="msg" :minLength=1 :noRows=5 :errorText="'Określ treść wiadomości.'" ref="msg"/>
+              </div>
+              <button class="contactform__button background__color--light">Wyślij wiadomość</button>
           </form>
         </article>
       </section>
@@ -19,31 +35,57 @@
 <script>
 import MainMenu from "@/components/MainMenu";
 import MainFooter from "@/components/MainFooter";
+import InputText from "@/components/InputText";
+import InputEmail from "@/components/InputEmail";
+import InputTextArea from "@/components/InputTextArea";
 
 export default {
   name: "contact",
-  components: { MainMenu, MainFooter },
+  components: { MainMenu, MainFooter, InputText,InputEmail, InputTextArea },
   data(){
     return{
       inputValid: false,
     }
   },
   methods: {
-    getValid(){
-      console.log(this.$refs['my-child'].isValid);
-    }
+
   }
 };
 </script>
 
 <style scoped>
+.contactform__button{
+  width: 100%;
+  cursor: pointer;
+  height: 2.2em;
+  padding: .35rem .75rem;
+  text-align: center;
+  vertical-align: middle;
+  font-weight: bolder;
+  line-height: 1.5;
+  /* background-color: rgb(255, 255, 255); */
+  border: 1px solid rgb(90, 90, 90);
+  color: rgb(90, 90, 90);
+  border-radius: .25rem;
+  -moz-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+  -ms-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+  -o-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+  -webkit-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+  transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+  
+}
+.contactform__button:hover{
+  color: rgb(251, 252, 253)!important;
+  background-color: rgb(90, 90, 90)!important;
+  box-shadow: 2px 2px 6px #999;
+}
 .contactform__label{
     margin-bottom: 0.5rem;
 }
 .contactform__group{
     display: flex;
     flex-direction: column;
-    margin-bottom: 1rem;
+    margin-bottom: .5rem;
 }
 .contactform{
     margin-top:1rem;
@@ -75,7 +117,7 @@ export default {
   margin-top: 19%;
   margin-left: 5%;
   margin-right: 5%;
-  margin-bottom: 3%;
+  margin-bottom: 2%;
   box-shadow: 4px 6px 20px #999;
   border-radius: 5px;
 }
@@ -94,7 +136,7 @@ export default {
   right: 0;
   position: absolute;
   z-index: -1;
-  height: 100vh;
+  min-height: 100vh;
 }
 @media only screen and (min-width: 640px){
   .container__section{
@@ -129,6 +171,7 @@ export default {
     margin-top: 5%;
     margin-left: 15%;
     margin-right: 15%;
+    margin-bottom: 1%;
   }
 }
 </style>
