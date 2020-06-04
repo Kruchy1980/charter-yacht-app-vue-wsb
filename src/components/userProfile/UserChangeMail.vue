25f7d3ebd40f7dc9bface0b7c3c9693300660878
Paweł Jarosiewicz <pawlojar@gmail.com>
2020-06-05 00:40:56 +0200

    handling basic user account

---
+++ src/components/userProfile/UserChangeMail.vue
@@ -0,0 +1,214 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal--backdrop">
+      <div class="modal" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription" >
+        
+        <header id="modalTitle" class="modal__header text__color--dark" >
+            <h5>Zmiana adresu e-mail</h5>
+          <button type="button" class="button--close text__color--dark" @click="close" aria-label="Close modal" >&times;</button>
+        </header>
+
+        <section id="modalBody" class="modal__body text__color--dark">
+            <div class="body__group">
+                <label class="body__label--bold" for="mail">Nowy adres e-mail:</label>
+                <InputEmail id="mail" ref="mail" :error-text="'Proszę podać poprawny adres e-mail.'" />
+            </div>
+            <p class="body__p">Podaj aktualne dane konta aby potwierdzić zmianę adresu e-mail.</p>
+            <div class="body__group">
+                  <label class="body__label" for="loginEmail">E-mail:</label>
+                  <InputEmail id="loginEmail" ref="loginEmail" :error-text="'Proszę podać poprawny adres e-mail.'" />
+              </div>
+              <div class="body__group">
+                  <label class="body__label" for="loginPassword">Hasło:</label>
+                  <InputPassword id="loginPassword" ref="loginPassword" :min-length="6" :error-text="'Hasło musi mieć minimum 6 znaków.'" />
+              </div>
+        </section>
+
+        <footer class="modal__footer">
+          <slot name="footer">
+            <button type="button" class="button--red" @click="changeMail" aria-label="Change mail" >Zatwierdź</button>
+            <button type="button" class="button--gray" @click="close" aria-label="Close modal" >Anuluj</button>
+          </slot>
+        </footer>
+
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import InputEmail from "@/components/InputEmail";
+import InputPassword from "@/components/InputPassword";
+
+export default {
+    name: 'userChangeMail',
+    components: { InputEmail, InputPassword },
+    methods: {
+        close() {
+            this.$emit('close');
+        },
+        //weryfikacja stanu kontrolki
+        changeMail(){
+            let valid = true; //zmienna pomocnicza
+            let newMail = this.$refs['mail'];
+            let oldMail = this.$refs['loginEmail'];
+            let pass = this.$refs['loginPassword'];
+            let controls = [newMail,oldMail,pass];
+            controls.forEach(element => {
+            if(!element.isValid){
+                valid=false;
+                element.setState();  //uruchamiamy alert na kontrolce
+            }
+            });
+            if(valid){
+                this.$emit('changeMail',newMail.value,oldMail.value,pass.value);
+            }
+        },
+    },
+}
+</script>
+
+<style scoped>
+.body__p{
+    margin-bottom: 0.5rem;
+    margin-top: 0.5rem;
+}
+.body__label,
+.body__label--bold{
+    margin-bottom: 0.25rem;
+    font-size: .9rem;
+}
+.body__label--bold{
+    font-weight: bold;
+}
+.body__group{
+    display: flex;
+    flex-direction: column;
+    margin-bottom: .25rem;
+}
+
+.modal--backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.modal {
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    min-width: 250px;
+}
+.modal__body {
+    position: relative;
+    padding: .5rem 1rem;
+    color: rgb(33, 37, 41);
+    font-size: 1em;
+}
+.modal__footer {
+    border-top: 1px solid #eee;
+    justify-content: flex-end;
+    padding: .5rem;
+    display: flex;
+    flex-direction: row
+}
+.modal__header{
+    padding-top: .5rem;
+    padding-left: .5rem;
+    padding-right: .5rem;
+    padding-bottom: .25rem;
+    display: flex;
+    flex-direction: row;
+    border-bottom: 1px solid #eee;
+    justify-content: space-between;
+}
+.button--close {
+    border: none;
+    font-size: 1.4rem;
+    padding-left: .5rem;
+    padding-right: .5rem;
+    cursor: pointer;
+    font-weight: bold;
+    color: rgb(60, 84, 180);
+    background: transparent;
+}
+.button--gray,
+.button--red{
+    width: auto;
+    cursor: pointer;
+    height: 2.1em;
+    padding: .4rem .75rem;
+    text-align: center;
+    vertical-align: middle;
+    font-weight: bolder;
+    font-size: 0.9em;
+    border-radius: .25rem;
+    margin-right: .5rem;
+    -moz-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+    -ms-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+    -o-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+    -webkit-transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+    transition: border-color .5s ease-in-out,box-shadow .15s ease-in-out;
+}
+.button--gray{
+    border: 1px solid rgb(108, 117, 125);
+    color: rgb(108, 117, 125);
+}
+.button--gray:hover{
+    color: rgb(251, 252, 253);
+    background-color: rgb(108, 117, 125);
+    box-shadow: 2px 2px 6px #999;
+}
+.button--red{
+    border: 1px solid rgb(255, 0, 0);
+    color: rgb(255, 0, 0);
+}
+.button--red:hover{
+    color: rgb(251, 252, 253);
+    background-color: rgb(255, 0, 0);
+    box-shadow: 2px 2px 6px #999;
+}
+/* wbudowane klasy Vue */
+.modal-fade-enter,
+.modal-fade-leave-active {
+    opacity: 0;
+}
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+    transition: opacity .5s ease
+}
+@media only screen and (min-width: 768px){
+    .body__label,
+    .body__label--bold{
+        margin-bottom: 0rem;
+        margin-right: 0.75rem;
+        width: fit-content;
+        min-width: 3rem;
+        text-align: right;
+    }
+    .body__group{
+        flex-direction: row;
+        align-items: baseline;
+    }
+    .modal{
+        min-width: 500px;
+        width: fit-content;
+    }
+    .modal__header{
+        padding-bottom: 0.2rem;
+        margin-bottom: 0.4rem;
+    }
+    .modal__footer{
+        padding-top: 0.75rem;
+        padding-bottom: 0.75rem;
+    }
+} 
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -12,20 +12,20 @@
           <div class="user__form__group">
             <label for="userName" class="user__form__label">Imię:</label>
             <div class="user__section__group">
-              <input v-model="currentUser.data.displayName" type="text" class="user__form__input text__color--dark" readonly>
-              <button v-tooltip.right="'zmień imię'" class="user__form__button"><i class="fas fa-edit"></i></button>
+              <input v-model="userName" type="text" class="user__form__input text__color--dark" readonly>
+              <button v-tooltip.right="'zmień imię'" @click="isUserChangeName=!isUserChangeName" class="user__form__button"><i class="fas fa-edit"></i></button>
             </div>
           </div>
           <div class="user__form__group">
             <label for="userName" class="user__form__label">E-mail:</label>
             <div class="user__section__group">
-              <input v-model="currentUser.data.email" type="text" class="user__form__input text__color--dark" readonly>
-              <button v-tooltip.right="'zmień e-mail'" class="user__form__button"><i class="fas fa-edit"></i></button>
+              <input v-model="userMail" type="text" class="user__form__input text__color--dark" readonly>
+              <button v-tooltip.right="'zmień e-mail'" @click="isUserChangeMail=!isUserChangeMail" class="user__form__button"><i class="fas fa-edit"></i></button>
             </div>
           </div>
           <div class="user__button__group">
-            <button class="user__button--red">Zmień hasło</button>
-            <button class="user__button--red">Usuń konto</button>
+            <button @click="isUserChangePass=!isUserChangePass" class="user__button--red">Zmień hasło</button>
+            <button @click="isUserDeleteAccount=!isUserDeleteAccount" class="user__button--red">Usuń konto</button>
           </div>
         </div>
       </div>
@@ -63,8 +63,16 @@
     <!-- wypełniacz okna dla usera, który nie jest zalogowany -->
     <div v-show="!isLoggedUser" class="container"></div>
     <!-- okna modalne -->
+    <ModalInfo v-show="isModalInfoVisible" :is-error="modalIsError" @close="closeModalInfo">
+      <template v-slot:header>{{modalTitle}}</template>
+      <template v-slot:body><div>{{modalMsg}}</div></template>
+    </ModalInfo>
     <UserModal v-show="isUserModalVisible" />
     <ModalLoading v-show="isLoadingVisible" />
+    <UserChangeName v-show="isUserChangeName" @close="isUserChangeName=!isUserChangeName" @changeName="changeUserName" />
+    <UserChangeMail v-show="isUserChangeMail" @close="isUserChangeMail=!isUserChangeMail" @changeMail="changeUserMail" />
+    <UserChangePass v-show="isUserChangePass" @close="isUserChangePass=!isUserChangePass" @changePass="changeUserPass" />
+    <UserDeleteAccount v-show="isUserDeleteAccount" @close="isUserDeleteAccount=!isUserDeleteAccount" @deleteAccount="userDeleteAccount" />
     <MainFooter />
   </div>
 </template>
@@ -78,16 +86,26 @@ import ModalLoading from "@/components/LoadingLineDots";
 import ModalInfo from "@/components/ModalInfo";
 import firebase from "@/firebase.js";
 import UserModal from "@/components/UserModal";
+import UserChangeName from "@/components/userProfile/UserChangeName";
+import UserChangeMail from "@/components/userProfile/UserChangeMail";
+import UserChangePass from "@/components/userProfile/UserChangePass";
+import UserDeleteAccount from "@/components/userProfile/UserDeleteAccount";
 import { mapState }  from "vuex";
 export default {
   name: "userAccount",
-  components: { MainMenu, MainFooter, UserModal, ModalLoading },
+  components: { MainMenu, MainFooter, UserModal, ModalLoading, ModalInfo, UserChangeName,UserChangeMail,UserChangePass,UserDeleteAccount },
   data() {
     return{
       isLoadingVisible: false,isModalInfoVisible: false, isUserModalVisible: true,
       modalTitle: '', modalMsg: '', 
       modalIsError: true,  //flaga określająca czy pokazywane okno modalne jest błędem
       isLoggedUser: false,
+      isUserChangeName: false,
+      isUserChangeMail: false,
+      isUserChangePass: false,
+      isUserDeleteAccount: false,
+      userName: '',
+      userMail: '',
     }
   },
   computed: {
@@ -100,6 +118,8 @@ export default {
       if(!this.currentUser.data.isAnonymous){
         this.isLoggedUser = true; //jest zalogowany użytkownik, i nie jest to użytkownik anonimowy używany do wyświetlania danych z bazy
         this.isUserModalVisible = false;
+        this.userName = this.currentUser.data.displayName;
+        this.userMail = this.currentUser.data.email;
       }
     }
   },
@@ -117,7 +137,144 @@ export default {
       b.getElementsByTagName("I").forEach(element => {
         element.classList.toggle('show');
       });
-
+    },
+    closeModalInfo() {
+      this.isModalInfoVisible = false;
+    },
+    showError(error,title){
+      this.modalIsError = true;
+      this.isLoadingVisible=false;
+      this.modalTitle=title+' '+error.code;
+      this.modalMsg=error.message;
+      console.error(this.modalTitle, this.modalMsg);
+      this.isModalInfoVisible = true;
+    },
+    changeUserName(newName){
+      const that = this;
+      try{
+        if(that.isLoggedUser){
+          that.isLoadingVisible = true; //animacja
+          let user = firebase.auth().currentUser; //bieżący user zalogowany
+          user.updateProfile({
+            displayName: newName
+          })
+          .then(()=>{
+            that.$store.dispatch("fetchUser", user);  //odświeżamy usera
+            that.userName = that.currentUser.data.displayName;
+            that.isLoadingVisible = false;  //zamykamy animację
+            that.isUserChangeName=false;  //zamykamy okno
+          })
+          .catch(function(error){
+            that.showError(error,'Błąd: ');
+          });
+        }
+      }
+      catch(err){
+        that.showError(err,'error change user name: ');
+      }
+    }, 
+    changeUserMail(newMail,oldMail,pass){
+      const that = this;
+      try{
+        if(that.isLoggedUser){
+          that.isLoadingVisible = true; //animacja
+          let user = firebase.auth().currentUser; //bieżący user zalogowany
+          //ponownie uwierzytelniamy usera
+          let credentials = firebase.auth.EmailAuthProvider.credential(oldMail,pass);
+          user.reauthenticateWithCredential(credentials)
+          .then(()=>{
+            user.updateEmail(newMail)
+            .then(()=>{
+              that.$store.dispatch("fetchUser", user);  //odświeżamy usera
+              that.userMail = that.currentUser.data.email;
+              that.isLoadingVisible = false;  //zamykamy animację
+              that.isUserChangeMail=false;  //zamykamy okno
+            })
+            .catch(function(error){
+              that.showError(error,'Błąd zmiany adresu e-mail: ');
+            });
+          })
+          .catch(function(error){
+            that.showError(error,'Błąd uwierzytelniania: ');
+          });
+        }
+      }
+      catch(err){
+        that.showError(err,'error change user mail: ');
+      }
+    },
+    changeUserPass(newPass,mail,oldPass){
+      const that = this;
+      try{
+        if(that.isLoggedUser){
+          if(that.isLoggedUser){
+            that.isLoadingVisible = true; //animacja
+            let user = firebase.auth().currentUser; //bieżący user zalogowany
+            //ponownie uwierzytelniamy usera
+            let credentials = firebase.auth.EmailAuthProvider.credential(mail,oldPass);
+            user.reauthenticateWithCredential(credentials)
+            .then(()=>{
+              user.updatePassword(newPass)
+              .then(()=>{
+                that.$store.dispatch("fetchUser", user);  //odświeżamy usera
+                that.isLoadingVisible = false;  //zamykamy animację
+                that.isUserChangePass=false;  //zamykamy okno
+                //komunikat z potwierdzeniem zmiany
+                that.modalIsError = false;  
+                that.modalTitle='Zmiana hasła';
+                that.modalMsg='Twoje hasło zostało zmienione.';
+                that.isModalInfoVisible = true;
+              })
+              .catch(function(error){
+                that.showError(error,'Błąd zmiany hasła: ');
+              });
+            })
+            .catch(function(error){
+              that.showError(error,'Błąd uwierzytelniania: ');
+            });
+          }
+        }
+      }
+      catch(err){
+        that.showError(err,'error change user password: ');
+      }
+    },
+    userDeleteAccount(mail,pass){
+      const that = this;
+      try{
+        if(that.isLoggedUser){
+          if(that.isLoggedUser){
+            that.isLoadingVisible = true; //animacja
+            let user = firebase.auth().currentUser; //bieżący user zalogowany
+            //ponownie uwierzytelniamy usera
+            let credentials = firebase.auth.EmailAuthProvider.credential(mail,pass);
+            user.reauthenticateWithCredential(credentials)
+            .then(()=>{
+              user.delete()
+              .then(()=>{
+                that.$store.dispatch("fetchUser", user);  //odświeżamy usera
+                that.isLoadingVisible = false;  //zamykamy animację
+                that.isUserDeleteAccount=false;  //zamykamy okno
+                //komunikat z potwierdzeniem zmiany
+                that.modalIsError = false;  
+                that.modalTitle='Usuwanie konta';
+                that.modalMsg='Twoje konto zostało usunięte.';
+                that.isModalInfoVisible = true;
+                setTimeout(() => that.$router.go(0), 5000); //odświeżamy bieżącą stronę po 5sek -> spowoduje załadowanie strony logowania
+              })
+              .catch(function(error){
+                that.showError(error,'Błąd usuwania konta: ');
+              });
+            })
+            .catch(function(error){
+              that.showError(error,'Błąd uwierzytelniania: ');
+            });
+          }
+        }
+      }
+      catch(err){
+        that.showError(err,'error delete user: ');
+      }
     }
   },
 };
